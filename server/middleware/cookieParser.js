6b185366d41d19531af8547581dc9a4b3399f743
Blake Crawford <blake.crawford@outlook.com>
2022-08-17 22:05:26 -0700

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,18 @@
 const parseCookies = (req, res, next) => {
+  const cookieString = req.headers.cookie;
+  if (cookieString === undefined) {
+    req.cookies = {};
+  } else {
+    let pairs = cookieString.split(';');
+    let splitPairs = pairs.map(cookieString => cookieString.split('='));
+    let cookieObj = {};
+
+    splitPairs.forEach((cookie) => {
+      cookieObj[cookie[0].trim()] = cookie[1];
+    });
+    req.cookies = cookieObj;
+  }
+  next();
 };
 
 module.exports = parseCookies;
